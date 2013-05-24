9f0d8d88eb8eee1149b039735727d013dc4c8265
Jens Dieskau <jfreax@googlemail.com>
2013-05-25 00:17:41 +0200

    Read evaluation values (alpha/beta/correct) from transposition table

--- src/player/millitta/AlphaBetaPruning.java
+++ src/player/millitta/AlphaBetaPruning.java
@@ -9,6 +9,9 @@ public class AlphaBetaPruning implements AlphaBetaPruningConstants {
     private int maxDepth;
     private long endTime;
 
+    private int currentBestMoveValue = -999; // TODO -infinity
+    private long currentBestMove = -1;
+
 
     public AlphaBetaPruning(long boardState, int maxDepth, int maxTime) {
         this.transpositionTable = new HashMap<Long, Long>();
@@ -18,14 +21,34 @@ public class AlphaBetaPruning implements AlphaBetaPruningConstants {
     }
 
     private int alphaBetaPruningSearch(long board, int alpha, int beta, int currentDepth, int remainingDepth) {
-        if( System.currentTimeMillis() > endTime ) {
+        if (System.currentTimeMillis() > endTime) {
             // TODO proper ending, set variable or something
             // TODO return something meaningful
             return 0;
         }
 
-        Long boardValue = transpositionTable.get(board);
-        if( boardValue != null ) {
+        Long boardComputedL = transpositionTable.get(board);
+        if (boardComputedL != null) {
+            long boardComputed = boardComputedL.longValue();
+            int boardValue = (int) (boardComputed & 2147483647); // (2^31)-1 aka first 31 bit
+
+            if ((boardComputed & (1L << BIT_HAS_CUT)) != 0) {
+                alpha = Math.max(alpha, boardValue);
+            }
+
+            if ((boardComputed & (1L << BIT_CAN_CUT_DEEPER)) != 0) {
+                beta = Math.min(beta, boardValue);
+            }
+
+            if (((boardComputed & (1L << BIT_HAS_CUT)) == 0 && (boardComputed & (1L << BIT_CAN_CUT_DEEPER)) == 0)
+                    || alpha >= beta) {
+                if (currentDepth == 0) {
+                    // TODO currentBestMove = get best move from boardComputed;
+                    currentBestMoveValue = boardValue;
+                }
+
+                return boardValue;
+            }
 
         }
 
--- src/player/millitta/AlphaBetaPruningConstants.java
+++ src/player/millitta/AlphaBetaPruningConstants.java
@@ -2,8 +2,8 @@ package player.millitta;
 
 
 public interface AlphaBetaPruningConstants {
-    int BIT_VALUE = 0; // -31
-    int BIG_REMAIN_DEPTH = 32;
-    int BIT_HAS_CUT = 33;
-    int BIT_CAN_CUT_DEEPER = 34;
+    int BIT_VALUE = 0; // -> 30
+    int BIG_REMAIN_DEPTH = 31;
+    int BIT_HAS_CUT = 32;
+    int BIT_CAN_CUT_DEEPER = 33;
 }
