b06277e6fdfadde2f5dcf48795fd5816bc9dabc6
Jens Dieskau <jfreax@googlemail.com>
2013-05-24 23:41:07 +0200

    New interface to save constants for alpha-beta

--- src/player/millitta/AlphaBetaPruning.java
+++ src/player/millitta/AlphaBetaPruning.java
@@ -4,23 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class AlphaBetaPruning {
-    private Map<Long, Long> transpositionTable;
+public class AlphaBetaPruning implements AlphaBetaPruningConstants {
+    private Map<Long, Long> transpositionTable; // BoardState, ComputedEvaluation
     private int maxDepth;
-    private int maxTime;
-    private long startTime;
+    private long endTime;
 
 
     public AlphaBetaPruning(long boardState, int maxDepth, int maxTime) {
         this.transpositionTable = new HashMap<Long, Long>();
 
         this.maxDepth = maxDepth;
-        this.maxTime = maxTime;
-
-        this.startTime = System.currentTimeMillis();
+        this.endTime = maxTime + System.currentTimeMillis();
     }
 
-    private int alphaBetaPruningSearch(int alpha, int beta, int currentDepth, int remainingDepth) {
+    private int alphaBetaPruningSearch(long board, int alpha, int beta, int currentDepth, int remainingDepth) {
+        if( System.currentTimeMillis() > endTime ) {
+            // TODO proper ending, set variable or something
+            // TODO return something meaningful
+            return 0;
+        }
+
+        Long boardValue = transpositionTable.get(board);
+        if( boardValue != null ) {
+
+        }
 
         return 0;
     }
---
+++ src/player/millitta/AlphaBetaPruningConstants.java
@@ -0,0 +1,9 @@
+package player.millitta;
+
+
+public interface AlphaBetaPruningConstants {
+    int BIT_VALUE = 0; // -31
+    int BIG_REMAIN_DEPTH = 32;
+    int BIT_HAS_CUT = 33;
+    int BIT_CAN_CUT_DEEPER = 34;
+}
