6d32b7d8596226b39e62a173369a8b1072ace14f
Jens Dieskau <jfreax@googlemail.com>
2013-05-24 23:22:28 +0200

    Write boilerplate for alpha beta pruning

--- src/player/millitta/AlphaBetaPruning.java
+++ src/player/millitta/AlphaBetaPruning.java
@@ -1,6 +1,27 @@
 package player.millitta;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class AlphaBetaPruning {
+    private Map<Long, Long> transpositionTable;
+    private int maxDepth;
+    private int maxTime;
+    private long startTime;
 
+
+    public AlphaBetaPruning(long boardState, int maxDepth, int maxTime) {
+        this.transpositionTable = new HashMap<Long, Long>();
+
+        this.maxDepth = maxDepth;
+        this.maxTime = maxTime;
+
+        this.startTime = System.currentTimeMillis();
+    }
+
+    private int alphaBetaPruningSearch(int alpha, int beta, int currentDepth, int remainingDepth) {
+
+        return 0;
+    }
 }
