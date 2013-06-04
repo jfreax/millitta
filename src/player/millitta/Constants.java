475dd388aca52e9db7668464532ed80054359a04
Jens Dieskau <jfreax@googlemail.com>
2013-06-04 22:43:54 +0200

    Game phase constants.

--- src/player/millitta/Constants.java
+++ src/player/millitta/Constants.java
@@ -5,6 +5,10 @@ public interface Constants {
     int BIT_PLAYER = 48;
     int BIT_GAMEPHASE = 49;
 
+    int GAMEPHASE_SETMAN = 0;
+    int GAMEPHASE_MOVEMAN = 1;
+    int GAMEPHASE_REMOVEMAN = 2;
+
     //int BIT_
     int WEIGHT_OPEN_MILL = 0;
     int WEIGHT_CLOSED_MILL = 1;
