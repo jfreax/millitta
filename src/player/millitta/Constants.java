6bc27a9806fd393e28063d1dc619025ba96b5ea6
Jens Dieskau <jfreax@googlemail.com>
2013-06-05 11:54:52 +0200

    New action & gamephase constants.

--- src/player/millitta/Constants.java
+++ src/player/millitta/Constants.java
@@ -3,12 +3,11 @@ package player.millitta;
 
 public interface Constants {
     long BIT_PLAYER = 48;
-    //int BIT_GAMEPHASE = 49;
     long BIT_ACTION = 49;
 
-    long GAMEPHASE_SETMAN = 0;
-    long GAMEPHASE_MOVEMAN = 1;
-    long GAMEPHASE_REMOVEMAN = 2;
+    long GAMEPHASE_SP = 0;
+    long GAMEPHASE_ZP = 1;
+    long GAMEPHASE_FP = 2;
 
     int WEIGHT_OPEN_MILL = 0;
     int WEIGHT_CLOSED_MILL = 1;
