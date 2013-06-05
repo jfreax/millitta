410cb6222ceed83d666256d12c10f06b3d99d9a6
Jens Dieskau <jfreax@googlemail.com>
2013-06-05 14:03:19 +0200

    No valid board magic number

--- src/player/millitta/Constants.java
+++ src/player/millitta/Constants.java
@@ -3,7 +3,11 @@ package player.millitta;
 
 public interface Constants {
     long BIT_PLAYER = 48;
-    long BIT_ACTION = 49;
+    long BIT_PHASE = 49;
+    long BIT_ACTION = 52;
+
+    long BITS_MENS1 = 16777215L; // TODO is this correct?
+    long BITS_MENS2 = 281474959933440L;
 
     long GAMEPHASE_SP = 0;
     long GAMEPHASE_ZP = 1;
@@ -11,6 +15,8 @@ public interface Constants {
 
     int WEIGHT_OPEN_MILL = 0;
     int WEIGHT_CLOSED_MILL = 1;
+
+    long MAGIC_NO_BOARD = -1L; // kennzeichnet ein invaliden oder nicht existenten board status
 
 
     double LOG2 = Math.log(2);
