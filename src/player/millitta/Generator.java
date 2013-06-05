04557d1c8f01e0d8a8512accd8463f6b9e2543ec
Jens Dieskau <jfreax@googlemail.com>
2013-06-05 14:04:02 +0200

    Get next boards for first game phase (untested)

--- src/player/millitta/Generator.java
+++ src/player/millitta/Generator.java
@@ -1,44 +1,72 @@
 package player.millitta;
 
-public class Generator implements Constants {
+import java.util.ArrayList;
 
-    private long board_ = 0L;
-    private int gamePhase = -1;
+public class Generator implements Constants, algds.Constants {
+
+    private long board_  = 0L;
+    private long action = -1L;
+    private long phase  = -1L;
+    private int playerID = -1;
+
+    //private ArrayList<Long> possNextBoards = null;
+    private static long[] nextBoards = new long[100]; // TODO find max. size
+    private int boardPointer = 0;
+
 
     Generator(long board) {
         board_ = board;
 
-        calcGamePhase();
+        calcAction();
+        calcPlayerID();
     }
 
-    private void calcGamePhase() {
-        if (
-                (board_ & (1L <<  BIT_GAMEPHASE))    != 0 &&
-                (board_ & (1L << (BIT_GAMEPHASE+1))) == 0 &&
-                (board_ & (1L << (BIT_GAMEPHASE+2))) == 0
-                ) {
-            gamePhase = GAMEPHASE_SETMAN;
-        } else if (
-                (board_ & (1L <<  BIT_GAMEPHASE))    == 0
-                ) {
-            gamePhase = GAMEPHASE_MOVEMAN;
-        } else if (
-                (board_ & (1L <<  BIT_GAMEPHASE))    != 0 &&
-                (board_ & (1L << (BIT_GAMEPHASE+1))) != 0 &&
-                (board_ & (1L << (BIT_GAMEPHASE+1))) != 0
-                ) {
-            gamePhase = GAMEPHASE_REMOVEMAN;
+
+    private void calcPlayerID() {
+        playerID = (board_ & (1L << BIT_PLAYER)) == 0 ? 0 : 1;
+    }
+
+
+    private void calcAction() {
+        if ((board_ & ((1L << BIT_ACTION) | (1L << (BIT_ACTION + 1)))) != 0L ) {
+            action = REMOVE_MAN;
+        } else if ((board_ & (1L << BIT_ACTION)) != 0L ) {
+            action = SET_MAN;
+        } else {
+            action = MOVE_MAN;
         }
     }
 
+    public long[] getNextBoards() {
+        boardPointer = 0;
 
-    public long getNextBoard() {
-
-        int playerID = (board_ & (1L << BIT_PLAYER)) == 0 ? 0 : 1;
+        if( (board_ & ((1L << BIT_PHASE) | (1L << (BIT_PHASE+1)))) != 0) { // Flugphase
 
 
+        } else if ((board_ & (1L << BIT_PHASE)) != 0) { // Setzphase
+            int rest = 9 - Helper.getMyMen(board_);
 
-        return 0L;
+            if ( rest <= 0 ) {
+                System.out.println("Something went wrong!\nGamephase says I have to set a man, but no men left to set :/");
+                return nextBoards;
+            }
+
+            long mergedBoardPoints = (board_ & BITS_MENS1) | ((board_ & BITS_MENS2) << 24);
+
+            for( int i = 0; i < 24; i++ ) {
+                if ((mergedBoardPoints & (1L << i)) == 0) {
+                    nextBoards[boardPointer++] = Helper.setMan(board_, i);
+                }
+            }
+
+        } else { // Zugphase
+
+        }
+
+        nextBoards[boardPointer] = MAGIC_NO_BOARD;
+        return nextBoards;
     }
+
+
 
 }
