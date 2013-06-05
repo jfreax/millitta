037443660d88906aa1afa95836b0d0d883fa5e08
Jens Dieskau <jfreax@googlemail.com>
2013-06-05 14:03:31 +0200

    More helper functions.

--- src/player/millitta/Helper.java
+++ src/player/millitta/Helper.java
@@ -4,9 +4,25 @@ package player.millitta;
 public class Helper implements Constants {
     static public int getMyMen(long board) {
         if ((board & (1L << BIT_PLAYER)) != 0) {
-            return Long.bitCount(board & 281474959933440L); // TODO is this correct?
+            return Long.bitCount(board & BITS_MENS2);
         } else {
-            return Long.bitCount(board & 16777215L);
+            return Long.bitCount(board & BITS_MENS2);
         }
+    }
+
+    static public int getFreePoss(long board) {
+        return 24 - Long.bitCount((board & BITS_MENS1) | ((board & BITS_MENS2) << 24));
+    }
+
+    static public long setMan(long board, int pos) {
+        if ((board & (1L << BIT_PLAYER)) != 0) {
+            return board | (pos << 24);
+        } else {
+            return board | pos;
+        }
+    }
+
+    static public void printBoard(long board) {
+
     }
 }
