71ed6a582687ef68b1110908c039046c72ea9cc0
Jens Dieskau <jfreax@googlemail.com>
2013-06-06 20:27:46 +0200

    Flying phase.

--- src/player/millitta/Generate/GeneratorFlyingPhase.java
+++ src/player/millitta/Generate/GeneratorFlyingPhase.java
@@ -12,6 +12,19 @@ public class GeneratorFlyingPhase extends AbstractGenerator {
         System.out.println("Generate: Flugphase");
         boardPointer = 0;
 
+        for (int i = 0; i < 24; i++) {
+            // Fuer jeden meiner Spielsteine
+            if (isMyMen(i)) {
+                // Teste jede andere position ...
+                for (int j = 0; j < 24; j++) {
+                    // ... ob sie noch frei ist.
+                    if (!isMen(j)) {
+                        nextBoards[boardPointer++] = moveMyMen(i, j);
+                    }
+                }
+            }
+        }
+
         nextBoards[boardPointer] = MAGIC_NO_BOARD;
         return nextBoards;
     }
