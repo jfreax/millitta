21e97f0935b12766213b503e4c252ebec1cceb9d
Jens Dieskau <jfreax@googlemail.com>
2013-06-26 17:40:25 +0200

    Exception when lost

---
+++ src/player/millitta/Search/YouLostException.java
@@ -0,0 +1,11 @@
+package player.millitta.Search;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jens
+ * Date: 26.06.13
+ * Time: 12:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class YouLostException extends Exception {
+}
